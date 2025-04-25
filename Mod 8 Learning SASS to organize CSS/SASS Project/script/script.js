429acac22747a48600f650c6be6dd04b249d858d
Stephen <145696963+anvgigz@users.noreply.github.com>
2025-04-25 15:13:59 -0500

    Add files via upload

---
+++ Mod 8 Learning SASS to organize CSS/SASS Project/script/script.js
@@ -0,0 +1,4 @@
+//email function
+function sendEmail() {
+    window.location.href = "mailto:Stephen.c.krohn@gmail.com?subject=Hello&body=Hello%20Stephen%20I%20saw%20your%20website%20and%20I%20wanted%20to%20reach%20out$20to%20you!";
+}
