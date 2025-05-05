e32d18262a7c9e2794c19b3e670f688fd08c9a7f
Stephen <145696963+anvgigz@users.noreply.github.com>
2025-05-05 16:08:07 -0500

    Add files via upload

---
+++ mod 10 working with Javascript/test its time for a change/script/script.js
@@ -0,0 +1,34 @@
+var largePic = document.getElementById('large');
+    var caption = document.getElementById('caption');
+
+function change01() {
+    largePic.src = "./images/CodingLogo.png";
+    largePic.alt = "Coding Logo";
+    caption.innerText = largePic.alt;
+}
+
+function change02() {
+    largePic.src = "./images/JavaScriptLogo.png";
+    largePic.alt = "JavaScript Logo";
+    caption.innerText = largePic.alt;
+}
+
+function change03() {
+    largePic.src = "./images/JSON Logo.png";
+    largePic.alt = "JSON Logo";
+    caption.innerText = largePic.alt;
+}
+
+
+function change04() {
+    largePic.src = "./images/Node.jsSymbol.png";
+    largePic.alt = "Node.js";
+    caption.innerText = largePic.alt;
+}
+
+function change05() {
+    largePic.src = "./images/MYSQL.png";
+    largePic.alt = "SQL";
+    caption.innerText = largePic.alt;
+}
+
